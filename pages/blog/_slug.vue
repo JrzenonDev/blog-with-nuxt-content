8a382fc7e5867ad74e73c612c82df7f5105ef447
JrzenonDev <jrobertoonb@gmail.com>
2021-07-26 23:58:59 -0300

    chore: create function format date

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -2,6 +2,7 @@
   <main>
     <article>
       <nuxt-content :document="article" />
+      <p>Post last update: {{ formatDate(article.updatedAt) }}</p>
     </article>
     <pre>{{ article }}</pre>
   </main>
@@ -14,6 +15,12 @@ export default {
     const article = await $content('articles', params.slug).fetch()
 
     return { article }
+  },
+  methods: {
+    formatDate (date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleString('pt-BR', options)
+    }
   }
 }
 </script>
